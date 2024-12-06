c88bb62980c87f37e350af9420e25960b1fd1055
32163555 <dlwnstjr0310@gmail.com>
2024-12-06 16:01:31 +0900

    init

---
+++ hub/src/main/kotlin/com/sparta/hub/HubApplication.kt
@@ -0,0 +1,11 @@
+package com.sparta.hub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HubApplication
+
+fun main(args: Array<String>) {
+    runApplication<HubApplication>(*args)
+}
