12077bcb463693cf0aac96b5343b43c09b871aa4
ksngh <chzhqk98@naver.com>
2024-12-14 02:57:43 +0900

    [#38] chore : settings for slack messaging service

---
+++ slack/src/main/java/com/sparta/slack/application/service/SlackServiceImpl.java
@@ -0,0 +1,6 @@
+package com.sparta.slack.application.service;
+
+import com.sparta.slack.domain.service.SlackService;
+
+public class SlackServiceImpl implements SlackService {
+}
---
+++ slack/src/main/java/com/sparta/slack/domain/service/SlackService.java
@@ -0,0 +1,7 @@
+package com.sparta.slack.domain.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SlackService {
+}
