7027c366e0337baa565114b94ea7a113f98d0a8d
ksngh <chzhqk98@naver.com>
2024-12-13 16:19:10 +0900

    [#34] chore : enable feign client

--- product/src/main/java/com/sparta/product/ProductApplication.java
+++ product/src/main/java/com/sparta/product/ProductApplication.java
@@ -3,10 +3,10 @@ package com.sparta.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableFeignClients
+@EnableFeignClients
 @EnableJpaAuditing
 public class ProductApplication {
 
