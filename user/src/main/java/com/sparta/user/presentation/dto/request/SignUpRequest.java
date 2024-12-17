a991657e562ed43dbcff30cc2962f2c54e9f0de4
kanghuijin <rkd8527@naver.com>
2024-12-17 21:22:03 +0900

    refactor: validation 추가

--- user/src/main/java/com/sparta/user/presentation/dto/request/SignUpRequest.java
+++ user/src/main/java/com/sparta/user/presentation/dto/request/SignUpRequest.java
@@ -1,6 +1,7 @@
 package com.sparta.user.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,14 +12,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpRequest {
 
-    @NotBlank(message = "ID은(는) 비워둘 수 없습니다.")
-    @Size(min = 3, max = 20, message = "최소 3자 이상, 최대 20자 이하입니다.")
+    @NotBlank(message = "ID는 비워둘 수 없습니다.")
+    @Size(min = 4, max = 10, message = "ID는 최소 4자 이상, 최대 10자 이하입니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "ID는 알파벳 소문자(a~z)와 숫자(0~9)만 포함할 수 있습니다.")
     private String username;
 
-    @NotBlank(message = "비밀번호은(는) 비워둘 수 없습니다.")
-    @Size(min = 4, message = "비밀번호는 최소 4자 이상 입력해주세요.")
+    @NotBlank(message = "비밀번호는 비워둘 수 없습니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자 이상, 최대 15자 이하입니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>])[a-zA-Z\\d!@#$%^&*(),.?\":{}|<>]+$",
+            message = "비밀번호는 알파벳 대소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+    )
     private String password;
 
-    @NotBlank(message = "Slack ID은(는) 비워둘 수 없습니다.")
+    @NotBlank(message = "Slack ID는 비워둘 수 없습니다.")
     private String slackId;
 }
